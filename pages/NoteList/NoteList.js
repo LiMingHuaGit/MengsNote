9f13f5d141cba636e40a2a0bfa95d223cd7d25f2
LIMINGHUAGIT <2290320152@qq.com>
2020-04-13 13:57:18 +0800

    增加日记列表页面js方法
    1.解决一次只能查询20条数据的问题
    2.解决上拉刷新无法清除之前页面旧数据的问题

--- pages/NoteList/NoteList.js
+++ pages/NoteList/NoteList.js
@@ -3,6 +3,7 @@ var util = require('../../utils/util.js');
 Page({
   data: {
    note:[],
+   note_nums:0//笔记数量
   },
   //onload查询数据
   onLoad(){
@@ -10,7 +11,42 @@ Page({
   },
   //下拉刷新
   onPullDownRefresh: function () {
-    this.onLoad();
+    wx.reLaunch({
+      url: '/pages/NoteList/NoteList'
+    });
+  },
+  /**
+     * 页面上拉触底事件的处理函数
+     */
+  onReachBottom: function () {
+    wx.showLoading({
+      title: '刷新中！',
+      duration: 1000
+    })
+
+    let x = this.data.note_nums + 20
+    console.log(x)
+    let old_data = this.data.note
+    DB.orderBy('time', 'desc').skip(x) // 限制返回数量为 20 条
+      .get()
+      .then(res => {
+        // 这里是从数据库获取文字进行转换 变换显示（换行符转换） 
+        res.data.forEach((item, i) => {
+          res.data[i].content = res.data[i].content.split('*hy*').join('\n');
+        })
+
+        // 利用concat函数连接新数据与旧数据
+        // 并更新emial_nums  
+        this.setData({
+          note: old_data.concat(res.data),
+          note_nums: x
+        })
+        console.log(res.data)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+    console.log('circle 下一页');
   },
 
   //跳转笔记详情页面
@@ -27,16 +63,15 @@ Page({
       url: '/pages/NoteContent/NoteContent?content=' + encodeURIComponent(content) + '&time=' + time + '&title=' + title + '&id=' + id + '&type=' + type + '&imgId=' + imgId,
     })
   },
-  //查询所有数据
+  //查询数据
   getData(){
-    var that = this ;
-    DB.get({
+    var that = this;
+    DB.orderBy('time', 'desc').skip(0).get({
       success(res) {
-        
         console.log("读数据成功", res.data)
         that.data.note = res.data.concat(that.data.note);
         that.setData({
-          'note': that.data.note 
+          'note': that.data.note, 
         });
         console.log(that.data.note)
       },
